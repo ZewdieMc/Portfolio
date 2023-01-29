6ed0d941fc154147c77531a3b48dd5394968647c
ZewdieMc <zewdiehabtie26@gmail.com>
2023-01-29 14:28:00 +0300

    Implement mobile menu items

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,2 +1,15 @@
-const body = document.querySelector('body');
-console.log(body)
+const humberger = document.querySelector('#mobile-menu');
+const menu = document.querySelector('.menu');
+const closeMenu = document.querySelector('#close-menu');
+const menuLinks = document.querySelectorAll('.menu-content a');
+
+const toggleMobileMenu = function() {
+  menu.classList.toggle('hidden');  
+}
+
+humberger.addEventListener('click', toggleMobileMenu );
+closeMenu.addEventListener('click', toggleMobileMenu );
+
+menuLinks.forEach(link =>{
+  link.addEventListener('click', toggleMobileMenu);
+});
