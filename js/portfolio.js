7c2a3136d93f776e2bf31d89fc937e1f3c445253
ZewdieMc <zewdiehabtie26@gmail.com>
2023-01-30 15:29:53 +0300

    Implement scrollspy alternative for desktop menu items

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,13 +1,37 @@
-const humberger = document.querySelector('#mobile-menu');
-const menu = document.querySelector('.menu');
-const closeMenu = document.querySelector('#close-menu');
-const menuLinks = document.querySelectorAll('.menu-content a');
+document.addEventListener('DOMContentLoaded', () => {
+  const humberger = document.querySelector('#mobile-menu');
+  const menu = document.querySelector('.menu');
+  const closeMenu = document.querySelector('#close-menu');
+  const menuLinks = document.querySelectorAll('.menu-content a');
 
-const toggleMobileMenu = () => menu.classList.toggle('hidden');
+  const toggleMobileMenu = () => menu.classList.toggle('hidden');
 
-humberger.addEventListener('click', toggleMobileMenu);
-closeMenu.addEventListener('click', toggleMobileMenu);
+  humberger.addEventListener('click', toggleMobileMenu);
+  closeMenu.addEventListener('click', toggleMobileMenu);
 
-menuLinks.forEach((link) => {
-  link.addEventListener('click', toggleMobileMenu);
-});
+  menuLinks.forEach((link) => {
+    link.addEventListener('click', toggleMobileMenu);
+  });
+
+  const sections = document.querySelectorAll('section');
+  const navLi = document.querySelectorAll('nav ul li a');
+
+  window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute('id');
+      }
+    });
+
+    navLi.forEach((li) => {
+      li.classList.remove('active');
+      if (li.classList.contains(current)) {
+        li.classList.add('active');
+      }
+    });
+  });
+}, false);
