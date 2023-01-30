45742318b9e3785a458d9f37ff904a858e0db9b8
ZewdieMc <zewdiehabtie26@gmail.com>
2023-01-30 12:58:51 +0300

    Add ESLint to project

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,13 +3,11 @@ const menu = document.querySelector('.menu');
 const closeMenu = document.querySelector('#close-menu');
 const menuLinks = document.querySelectorAll('.menu-content a');
 
-const toggleMobileMenu = function() {
-  menu.classList.toggle('hidden');  
-}
+const toggleMobileMenu = () => menu.classList.toggle('hidden');
 
-humberger.addEventListener('click', toggleMobileMenu );
-closeMenu.addEventListener('click', toggleMobileMenu );
+humberger.addEventListener('click', toggleMobileMenu);
+closeMenu.addEventListener('click', toggleMobileMenu);
 
-menuLinks.forEach(link =>{
+menuLinks.forEach((link) => {
   link.addEventListener('click', toggleMobileMenu);
 });
