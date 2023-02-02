368a9a2c6f6b9d71e13d7fd4d16ad15419b190df
ZewdieMc <zewdiehabtie26@gmail.com>
2023-02-02 13:18:03 +0300

    Fix bugs in the modal scroll event

--- js/portfolio.js
+++ js/portfolio.js
@@ -34,7 +34,7 @@ const projects = [
       'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
       'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
     ],
-    technologies: ['html', 'css', 'JavaScript', 'github', 'Bootstrap', 'Ruby'],
+    technologies: ['html', 'css', 'JavaScript'],
     roles: [['CANOPY', 'CANOPY'], './images/Counter.png', ['Back End Dev', 'Back End Dev'], ['2015', '2015']],
     liveLink: 'Zewdiemc.github.io/Portfolio/',
     sourceLink: 'github.com/Zewdiemc/Portfolio',
@@ -46,7 +46,7 @@ const projects = [
       'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
       'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     ],
-    technologies: ['html', 'css', 'JavaScript', 'github', 'Bootstrap', 'Ruby',
+    technologies: ['html', 'Ruby on rails', 'css', 'JavaScript',
     ],
     roles: [['CANOPY', 'FACEBOOK'], './images/Counter.png', ['Back End Dev', 'Full Stack Dev'], ['2015', '2015']],
     liveLink: 'Zewdiemc.github.io/Portfolio/',
@@ -59,7 +59,7 @@ const projects = [
       'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
       'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     ],
-    technologies: ['html', 'css', 'JavaScript', 'github', 'Bootstrap', 'Ruby',
+    technologies: ['html', 'Ruby on rails', 'css', 'JavaScript',
     ],
     roles: [['CANOPY', 'FACEBOOK'], './images/Counter.png', ['Back End Dev', 'Full Stack Dev'], ['2015', '2015']],
     liveLink: 'Zewdiemc.github.io/Portfolio/',
@@ -72,11 +72,11 @@ const projects = [
       'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
       'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
     ],
-    technologies: ['html', 'css', 'JavaScript', 'github', 'Bootstrap', 'Ruby',
+    technologies: ['html', 'Ruby on rails', 'css', 'JavaScript',
     ],
     roles: [['CANOPY', 'Uber'], './images/Counter.png', ['Back End Dev', 'Lead Developer'], ['2015', '2018']],
-    liveLink: 'Zewdiemc.github.io/Portfolio/',
-    sourceLink: 'github.com/Zewdiemc/Portfolio',
+    liveLink: 'https://Zewdiemc.github.io/Portfolio/',
+    sourceLink: 'https://github.com/Zewdiemc/Portfolio',
   },
 ];
 
@@ -84,100 +84,120 @@ const projects = [
 const projectSection = document.querySelector('.works');
 const projectCards = projects.map(
   (project, i) => `
-  <div class="card">
-<img class='snapshoot mobile' src='${project.images[0]}'alt='background Image'>
-<img class='snapshoot desktop' src='${project.images[1]}'alt='background Image'>
-<div class='left_block'>
-  <div class="left_block_title">
-<h2>${project.name[0]}</h2>
-<div class='frame'>
-  <div class="client mobile">${project.roles[0][0]}</div>
-  <div class="client desktop">${project.roles[0][1]}</div>
-  <img class="counter" src="${project.roles[1]}" alt="work role">
-  <div class="role mobile">${project.roles[2][0]}</div>
-  <div class="role desktop">${project.roles[2][1]}</div>
-  <img class="counter" src="${project.roles[1]}" alt="work role">
-  <div class="year mobile">${project.roles[3][0]}</div>
-  <div class="year desktop">${project.roles[3][1]}</div>
-</div>
-<p class="description mobile">${project.description[0]}</p>
-<p class="description desktop">${project.description[1]}</p>
-<ul class="tags">
-  <li>${project.technologies[0]}</li>
-  <li class='desktop'>${project.technologies[1]}</li>
-  <li>${project.technologies[2]}</li>
-  <li>${project.technologies[3]}</li>
-</ul>
-<button data-target='${i}'class='action project-pop-btn'>See Project </button>
+<div class="card">
+  <img class='snapshoot mobile' src='${project.images[0]}'alt='background Image'>
+  <img class='snapshoot desktop' src='${project.images[1]}'alt='background Image'>
+  <div class='left_block'>
+    <div class="left_block_title">
+    <h2>${project.name[0]}</h2>
+    <div class='frame'>
+    <div class="client mobile">${project.roles[0][0]}</div>
+    <div class="client desktop">${project.roles[0][1]}</div>
+    <img class="counter" src="${project.roles[1]}" alt="work role">
+    <div class="role mobile">${project.roles[2][0]}</div>
+    <div class="role desktop">${project.roles[2][1]}</div>
+    <img class="counter" src="${project.roles[1]}" alt="work role">
+    <div class="year mobile">${project.roles[3][0]}</div>
+    <div class="year desktop">${project.roles[3][1]}</div>
+  </div>
+  <p class="description mobile">${project.description[0]}</p>
+  <p class="description desktop">${project.description[1]}</p>
+  <ul class="tags">
+    <li>${project.technologies[0]}</li>
+    <li class='desktop'>${project.technologies[1]}</li>
+    <li class='mobile'>${projects[0].technologies[1]}</li>
+    <li class='desktop'>${project.technologies[2]}</li>
+    <li class='mobile'>${projects[0].technologies[2]}</li>
+    <li class='desktop'>${project.technologies[3]}</li>
+  </ul>
+  <button data-target='${i}'class='action project-pop-btn'>See Project </button>
   </div>
 </div>
   </div>
 `,
 );
-  // Dynamically add the cards to the DOM
+
+// Dynamically add the cards to the DOM
 for (let i = 0; i < projectCards.length; i += 1) {
   projectSection.innerHTML += projectCards[i];
 }
 
-const modal = document.createElement('div');
-modal.classList.add('modal');
-modal.classList.add('hidden');
-modal.innerHTML = `
-<div class='modal-header'>
-  <h2></h2>
-  <a class='close-modal'>&times; </a>
-</div>
-<div class='frame'>
-  <div class="client mobile"></div>
-  <div class="client desktop"></div>
-  <img class="counter" src="" alt="work role">
-  <div class="role mobile"></div>
-  <div class="role desktop"></div>
-  <img class="counter" src="" alt="work role">
-  <div class="year mobile"></div>
-  <div class="year desktop"></div>
-</div>
-<img class='snapshoot mobile'src="" alt="project feature image">
-<img class='snapshoot desktop'src="" alt="project feature image">
-<div class='description_wrapper'>
-  <p class="description"></p>
-  <ul class="tags">
-    <li></li>
-    <li></li>
-    <li></li>
-    <li class='desktop'></li>
-    <li class='desktop'></li>
-    <li class='desktop'></li>
-  </ul>
-</div>
-`;
+// select the first card and remove the last li item from the ul
+const lastTechnology = document.querySelector('.card:first-child .tags li:last-child');
+if (lastTechnology.textContent === 'undefined') lastTechnology.remove();
 
-function closeModal() {
-  modal.style.display = 'none';
-}
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
 
-function showModal() {
-  modal.style.display = 'block';
-}
+const closeModal = () => {
+  modal.classList.toggle('hide');
+  overlay.classList.toggle('hide');
+  body.style.overflow = 'auto';
+};
 
-function updateModalContent(projectId) {
+const showModal = () => {
+  modal.classList.toggle('hide');
+  overlay.classList.toggle('hide');
+  body.style.overflow = 'hidden';
+};
+
+function updateModalContent(id) {
+  const description = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`;
+  const descriptionmob = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+   only five centuries, but also the leap into electronic typesetting, remaining essent`;
   modal.innerHTML = `
   <div class='modal-header'>
-    <h2>${projects[projectId].name[0]}</h2>
+    <h2 class='mobile'>${projects[id].name[0]}</h2>
+    <h2 class='desktop'>${projects[id].name[0]}</h2>
     <a class='close-modal'>&times; </a>
   </div>
-  `;
+  <div class='modal-content'>
+  <div class='frame'>
+    <div class="client mobile">${projects[id].roles[0][0]}</div>
+    <div class="client desktop">${projects[id].roles[0][1]}</div>
+    <img class="counter" src="${projects[id].roles[1]}" alt="work role">
+    <div class="role mobile">${projects[id].roles[2][0]}</div>
+    <div class="role desktop">${projects[id].roles[2][1]}</div>
+    <img class="counter" src="${projects[id].roles[1]}" alt="work role">
+    <div class="year mobile">${projects[id].roles[3][0]}</div>
+    <div class="year desktop">${projects[id].roles[3][1]}</div>
+</div>
+<img class='snapshoot mobile'src="${projects[id].images[0]}" alt="project feature image">
+<img class='snapshoot desktop'src="${projects[id].images[1]}" alt="project feature image">
+<div class='description_wrapper'>
+  <p class="description desktop">${description}</p>
+  <p class="description mobile">${descriptionmob}</p>
+  <div class='tags-button_wrapper'>
+      <ul class="tags">
+       <li>HTML</li>
+       <li>css</li>
+       <li>JavaScript</li>
+       <li class='desktop'>github</li>
+       <li class='desktop'>ruby</li>
+       <li class='desktop'>Bootstrap</li>
+      </ul>
+      <hr/  class='mobile'>
+    <div class='live-source-container'>
+      <a type='button'class='button live-link' href='https://${projects[id].liveLink}' target='_blank'>See Live <img src='./images/Icon-external-link.svg' alt='external link icon'></a>
+      <a type='button'class='button source-link' href='https://${projects[id].sourceLink}' target='_blank'>See Source <img src='./images/Icon-github.svg' alt='github icon'></a>
+    </div>
+  </div>
+</div>
+</div>
+`;
 }
+const projectDetailClickHandler = (projectId) => {
+  updateModalContent(projectId);
+  showModal();
+  document.querySelector('.close-modal').addEventListener('click', closeModal);
+};
 
-// add the modal to the DOM
-body.appendChild(modal);
 const popupBtns = document.querySelectorAll('.project-pop-btn');
-const closeModalBtn = document.querySelector('.close-modal');
-closeModalBtn.addEventListener('click', () => { closeModal(); });
 popupBtns.forEach((btn) => {
-  // const projectId = btn.getAttribute('data-target');
-  // updateModalContent(projectId);
-  btn.addEventListener('click', () => {
-    showModal();
-  });
+  const projectId = btn.getAttribute('data-target');
+  btn.addEventListener('click', () => projectDetailClickHandler(projectId));
 });
+
+// close modal when clicked outside
+overlay.addEventListener('click', closeModal);
