932577b0b51f1693d593118e8db5e049f4e283b0
ZewdieMc <zewdiehabtie26@gmail.com>
2023-02-03 16:27:37 +0300

    Use setTimeout on the error display

--- js/validate.js
+++ js/validate.js
@@ -13,8 +13,8 @@ form.addEventListener('submit', (event) => {
   if (email.value !== email.value.toLowerCase()) {
     emailErrorContainer.textContent = 'You are required to use only lowercase letters in your email.';
     emailErrorContainer.classList.add('active');
+    setTimeout( ()=> emailErrorContainer.classList.remove('active'), 5000);
   } else {
-    emailErrorContainer.classList.remove('active');
     form.submit();
   }
 });
