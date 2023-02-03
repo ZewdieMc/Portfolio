8a3dbadfe5b4a1dcffea704d84582787cacce082
Amanuel Belete <amanuelbelete95@gmail.com>
2023-02-03 15:39:32 +0300

    Update the validate.js file

--- js/validate.js
+++ js/validate.js
@@ -2,14 +2,13 @@ const form = document.querySelector('form');
 const email = document.getElementById('email');
 const emailErrorContainer = document.querySelector('span.error');
 
-// remove the active class from the error span when the user types
 email.addEventListener('input', () => {
   if (emailErrorContainer.classList.contains('active')) {
     emailErrorContainer.classList.remove('active');
   }
 });
 
-form.onsubmit = (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   if (email.value !== email.value.toLowerCase()) {
     emailErrorContainer.textContent = 'You are required to use only lowercase letters in your email.';
@@ -18,4 +17,5 @@ form.onsubmit = (event) => {
     emailErrorContainer.classList.remove('active');
     form.submit();
   }
-};
+});
+
