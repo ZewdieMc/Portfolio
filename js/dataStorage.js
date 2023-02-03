8d49435351eaa98efe7ea4351d22eed75c9be214
ZewdieMc <zewdiehabtie26@gmail.com>
2023-02-04 00:43:51 +0300

    Save and populate form data to and from local storage

---
+++ js/dataStorage.js
@@ -0,0 +1,29 @@
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+const populateForm = () => {
+  const userData = JSON.parse(localStorage.getItem('userData'));
+  username.value = userData.username;
+  email.value = userData.email;
+  message.value = userData.message;
+};
+
+const saveData = () => {
+  const userData = {
+    username: username.value,
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('userData', JSON.stringify(userData));
+};
+
+window.onload = () => {
+  if (localStorage.getItem('userData')) {
+    populateForm();
+  }
+};
+
+username.addEventListener('input', saveData);
+email.addEventListener('input', saveData);
+message.addEventListener('input', saveData);
